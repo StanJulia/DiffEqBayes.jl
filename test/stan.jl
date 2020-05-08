ce154143c0b3a13201ced3902f4d09c08be141bb
Rob J Goedman <goedman@mac.com>
2020-05-08 08:05:05 -0400

    Further CI tests

--- test/stan.jl
+++ test/stan.jl
@@ -12,20 +12,20 @@ p = [1.5]
 prob1 = ODEProblem(f1,u0,tspan,p)
 sol = solve(prob1,Tsit5())
 t = collect(range(1,stop=10,length=10))
-randomized = VectorOfArray([(sol(t[i]) + .01randn(2)) for i in 1:length(t)])
+randomized = VectorOfArray([(sol(t[i]) + .2randn(2)) for i in 1:length(t)])
 data = convert(Array,randomized)
 priors = [truncated(Normal(1.0,0.5),0.1,2)]
 
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=300,
-                                 num_warmup=500,likelihood=Normal)
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=2000,
+                                 num_warmup=1000,likelihood=Normal)
 
 sdf  = CmdStan.read_summary(bayesian_result.model)
 @test sdf[sdf.parameters .== :theta1, :mean][1] ≈ 1.5 atol=3e-1
 
 
-priors = [Normal(1.,0.01),Normal(1.,0.01),Normal(1.5,0.01)]
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=300,
-                                 num_warmup=500,likelihood=Normal,sample_u0=true)
+priors = [Normal(1.,0.5),Normal(1.,0.5),Normal(1.5,0.5)]
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=2000,
+                                 num_warmup=1000,likelihood=Normal,sample_u0=true)
 
 sdf  = CmdStan.read_summary(bayesian_result.model)
 @test sdf[sdf.parameters .== :theta1, :mean][1] ≈ 1. atol=3e-1
@@ -35,17 +35,17 @@ sdf  = CmdStan.read_summary(bayesian_result.model)
 sol = solve(prob1,Tsit5(),save_idxs=[1])
 randomized = VectorOfArray([(sol(t[i]) + .01 * randn(1)) for i in 1:length(t)])
 data = convert(Array,randomized)
-priors = [truncated(Normal(1.5,0.1),0.1,2)]
+priors = [truncated(Normal(1.5,0.5),0.1,2)]
 bayesian_result = stan_inference(prob1,t,data,priors;num_samples=2000,
-                                 num_warmup=500,likelihood=Normal,save_idxs=[1])
+                                 num_warmup=1000,likelihood=Normal,save_idxs=[1])
 
 sdf  = CmdStan.read_summary(bayesian_result.model)
 @test sdf[sdf.parameters .== :theta1, :mean][1] ≈ 1.5 atol=3e-1
 
 
-priors = [Normal(1.,0.01),Normal(1.5,0.01)]
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=300,
-                                 num_warmup=500,likelihood=Normal,save_idxs=[1],sample_u0=true)
+priors = [Normal(1.,0.5),Normal(1.5,0.5)]
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=2000,
+                                 num_warmup=1000,likelihood=Normal,save_idxs=[1],sample_u0=true)
 
 sdf  = CmdStan.read_summary(bayesian_result.model)
 @test sdf[sdf.parameters .== :theta1, :mean][1] ≈ 1. atol=3e-1
@@ -67,7 +67,8 @@ data = convert(Array,randomized)
 priors = [truncated(Normal(1.5,0.5),0.1,2),truncated(Normal(1.0,0.5),0.1,1.5),
           truncated(Normal(3.0,0.5),0.1,4),truncated(Normal(1.0,0.5),0.1,2)]
 
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=100,num_warmup=500,vars =(DiffEqBayes.StanODEData(),InverseGamma(4,1)))
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=2000,
+                                num_warmup=1000,vars =(DiffEqBayes.StanODEData(),InverseGamma(4,1)))
 sdf  = CmdStan.read_summary(bayesian_result.model)
 @test sdf[sdf.parameters .== :theta1, :mean][1] ≈ 1.5 atol=1e-1
 @test sdf[sdf.parameters .== :theta2, :mean][1] ≈ 1.0 atol=1e-1
